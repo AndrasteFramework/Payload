64b23e582948da41cacd4b2a059429bbb4f7c9cb
MeFisto94 <MeFisto94@users.noreply.github.com>
2021-05-27 01:07:15 +0200

    Added a preliminary ImGui Integration for DirectX 9 by shipping the required files (the NuGet package is built for the wrong version, so we're using https://github.com/AndrasteFramework/DearImguiSharp/commit/9262058fda322beda8fa49a7efc5ad2335619e8a). Currently clicking anything in the ShowAboutWindow causes one application to crash, pending investigation

---
+++ ImGuiIntegration/ImGuiManager.cs
@@ -0,0 +1,66 @@
+﻿#if !NETSTANDARD
+using System;
+using Andraste.Payload.D3D9;
+using Andraste.Shared.Lifecycle;
+using DearImguiSharp;
+
+namespace Andraste.Payload.ImGuiIntegration
+{
+    public class ImGuiManager : IManager
+    {
+        private readonly D3D9HookManager _d3d9;
+        private readonly IntPtr _hWnd;
+        private ImGuiContext _ctx;
+
+        public ImGuiManager(D3D9HookManager d3d9, IntPtr hWnd)
+        {
+            _d3d9 = d3d9;
+            _hWnd = hWnd;
+        }
+
+        public bool Enabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool Loaded => _ctx != null;
+
+        public void Load()
+        {
+            _ctx = ImGui.CreateContext(null);
+            var io = ImGui.GetIO();
+            ImGui.StyleColorsDark(null);
+            ImGui.ImGuiImplWin32Init(_hWnd);
+
+            unsafe
+            {
+                ImGui.ImGuiImplDX9Init((void*)(IntPtr)_d3d9.Device);
+            }
+
+            _d3d9.EndScene += device =>
+            {
+                //if (ImGui.IsKeyPressed((int)ImGuiKey.Space))
+                if (true || io.KeysDown[0x79]) // F10
+                {
+                    ImGui.ImGuiImplDX9NewFrame();
+                    ImGui.ImGuiImplWin32NewFrame();
+                    ImGui.NewFrame();
+                    //ImGui.ShowDemoWindow();
+                    bool open = true;
+
+                    ImGui.ShowAboutWindow(ref open);
+                    ImGui.EndFrame();
+
+                    ImGui.Render();
+                    ImGui.ImGuiImplDX9RenderDrawData(ImGui.GetDrawData());
+                }
+            };
+        }
+
+        public void Unload()
+        {
+            ImGui.ImGuiImplDX9Shutdown();
+            ImGui.ImGuiImplWin32Shutdown();
+            ImGui.DestroyContext(_ctx);
+            _ctx = null;
+        }
+    }
+}
+#endif
