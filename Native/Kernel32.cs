66184de5700121b1d37ca2ef2e387952c6144fe8
MeFisto94 <MeFisto94@users.noreply.github.com>
2021-05-28 18:32:20 +0200

    Added a DebugUtil to aid finding purecalls

---
+++ Native/Kernel32.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Andraste.Payload.Native
+{
+    public static class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern void DebugBreak();
+    }
+}
---
+++ Util/DebugUtil.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Andraste.Payload.Native;
+
+namespace Andraste.Payload.Util
+{
+    /// <summary>
+    /// A Utility Class to help debugging and writing against the framework.
+    /// </summary>
+    public static class DebugUtil
+    {
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void PureCallHandlerDelegate();
+        [DllImport("msvcr90.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "_set_purecall_handler")]
+        private static extern IntPtr _set_purecall_handler90([MarshalAs(UnmanagedType.FunctionPtr)] PureCallHandlerDelegate handler);
+
+        /// <summary>
+        /// Install a purecall handler for an <c>msvcr90.dll</c> application.
+        /// When a call of a pure virtual function happens, the default
+        /// behavior is only showing a small message box and then quitting.
+        ///
+        /// This Handler will emit a native breakpoint, so you can inspect
+        /// the callstack of that purecall. One can also put a
+        /// ".net breakpoint" on <see cref="PureCallHandler"/>
+        /// </summary>
+        public static void InstallPurecallHandler90()
+        {
+            _set_purecall_handler90(PureCallHandler);
+        }
+
+        private static void PureCallHandler()
+        {
+            // Place a Breakpoint here for .net Debugging
+            Kernel32.DebugBreak(); // Causes a native breakpoint
+        }
+    }
+}
